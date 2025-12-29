ff718eb557b0d5923cecadcd53a6633c59de7028
Muhammad Yusuf Tri Daryanto <L200220140@student.ums.ac.id>
2025-12-29 19:00:02 +0700

    Prepare deploy ke Railway
    tambah Procfile, gunicorn, whitenoise

--- Pendeteksi_Judol/Pendeteksi_Judol/settings.py
+++ Pendeteksi_Judol/Pendeteksi_Judol/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv("DJANGO_DEBUG", "0")))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Pendeteksi_Judol.urls'
@@ -142,3 +143,6 @@ STATIC_ROOT = BASE_DIR/"staticfiles"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
