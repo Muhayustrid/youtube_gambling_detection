df3eefd91103776962b0c2bf48cfd1073355b380
Muhammad Yusuf Tri Daryanto <L200220140@student.ums.ac.id>
2026-01-07 04:21:48 +0700

    Update settings.py

--- Pendeteksi_Judol/Pendeteksi_Judol/settings.py
+++ Pendeteksi_Judol/Pendeteksi_Judol/settings.py
@@ -170,6 +170,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'https://www.antislot.my.id', 'https://*.antislot.my.id']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
